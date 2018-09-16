ce173f2de893238b99afc1e482fb689492f27fce
christopheralan88 <christopheralan88@gmail.com>
2018-09-16 00:28:38 -0400

    Refactoring tanakh-printer.py (again) to be less repetitive.  Added times.ttf files.

--- tanakh-printer.py
+++ tanakh-printer.py
@@ -1,14 +1,39 @@
 from fpdf import FPDF
 from DbActions import DbActions
+from constants import Constants
 
 
-PEI = 'פ'
+def print_line_of_words_right_to_left(pdf, font, font_size, cell_w, cell_h, words, reverse_word=False):
+    while pdf.get_x() > pdf.l_margin:
+        pdf.set_font(font, '', font_size)
+
+        try:
+            for j in range(0, 5, 1):
+                word = words[j][0][::-1] if reverse_word else words[j][0]
+                pdf.cell(cell_w, cell_h, txt=word)
+                pdf.set_x(pdf.get_x() - cell_w - cell_w)  # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
+
+            # Get remainder and write cell with word
+            last_cell_w = pdf.get_x() - pdf.l_margin
+            word = words[5][0][::-1] if reverse_word else words[5][0]
+            pdf.cell(last_cell_w, cell_h, txt=word)
+            pdf.set_x(pdf.get_x() - last_cell_w - last_cell_w)
+        # If we reach the end of the list before finishing the loop, then just break the loop.
+        except (AttributeError, IndexError):
+            break
+
+
+def move_down_one_line(cell_h, x_origin):
+    pdf.set_y(pdf.get_y() + cell_h)
+    pdf.set_x(x_origin)
+
 
 pdf = FPDF()
 pdf.add_page()
-font = 'SBL_Hbrw'
+
+#font = 'SBL_Hbrw'
+font = 'times'
 pdf.add_font(font, '', '{}.ttf'.format(font), uni=True)
-pdf.set_font(font, '', 20)
 
 cell_w = 30
 cell_h = 10
@@ -16,79 +41,23 @@ x_origin = pdf.w - pdf.r_margin - cell_w
 pdf.set_x(x_origin)
 
 db_actions = DbActions()
-eng_tran_words = db_actions.get_eng_tran_by_book('genesis')
-heb_text_words = db_actions.get_heb_word_by_book('genesis')
-strongs_ref_words = db_actions.get_strongs_ref_by_book('genesis')
-heb_meaning_words = db_actions.get_heb_meaning_by_book('genesis')
-
-for i in range(0, len(heb_text_words), 6):
-
-    while pdf.get_x() > pdf.l_margin:
-        # Decrease font
-        pdf.set_font(font, '', 10)
-
-        try:
-
-            for j in range(0, 5, 1):
-                pdf.cell(cell_w, cell_h, txt=strongs_ref_words[i+j][0])
-                pdf.set_x(pdf.get_x() - cell_w - cell_w)  # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
-
-            # Get remainder and write cell with word
-            last_cell_w = pdf.get_x() - pdf.l_margin
-            pdf.cell(last_cell_w, cell_h, txt=strongs_ref_words[i+5][0], border=0)
-            pdf.set_x(pdf.get_x() - last_cell_w - last_cell_w) # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
-
-        except (AttributeError, IndexError):
-            break
-
-    # Go to down one line and set cursor at right side of page.
-    pdf.set_y(pdf.get_y() + cell_h)
-    pdf.set_x(x_origin)
 
 
-    while pdf.get_x() > pdf.l_margin:
-        # Increase font
-        pdf.set_font(font, '', 20)
+for book in Constants().torah:
+    eng_tran_words = db_actions.get_eng_tran_by_book(book)
+    heb_text_words = db_actions.get_heb_word_by_book(book)
+    strongs_ref_words = db_actions.get_strongs_ref_by_book(book)
+    heb_meaning_words = db_actions.get_heb_meaning_by_book(book)
 
-        try:
+    for i in range(0, len(heb_text_words), 6):
+        print_line_of_words_right_to_left(pdf, font, 10, cell_w, cell_h, strongs_ref_words[i:i+6])
+        move_down_one_line(cell_h, x_origin)
 
-            for j in range(0, 5, 1):
-                pdf.cell(cell_w, cell_h, txt=heb_text_words[i + j][0][::-1])
-                pdf.set_x(pdf.get_x() - cell_w - cell_w)  # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
+        print_line_of_words_right_to_left(pdf, font, 20, cell_w, cell_h, heb_text_words[i:i+6], True)
+        move_down_one_line(cell_h, x_origin)
 
-            # Get remainder and write cell with word
-            last_cell_w = pdf.get_x() - pdf.l_margin
-            pdf.cell(last_cell_w, cell_h, txt=heb_text_words[i+5][0][::-1], border=0)
-            pdf.set_x(pdf.get_x() - last_cell_w - last_cell_w) # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
-
-        except (AttributeError, IndexError):
-            break
-
-    # Go to down one line and set cursor at right side of page.
-    pdf.set_y(pdf.get_y() + cell_h)
-    pdf.set_x(x_origin)
-
-    while pdf.get_x() > pdf.l_margin:
-        # Decrease font
-        pdf.set_font(font, '', 10)
-
-        try:
-
-            for j in range(0, 5, 1):
-                pdf.cell(cell_w, cell_h, txt=eng_tran_words[i+j][0])
-                pdf.set_x(pdf.get_x() - cell_w - cell_w)  # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
-
-            # Get remainder and write cell with word
-            last_cell_w = pdf.get_x() - pdf.l_margin
-            pdf.cell(last_cell_w, cell_h, txt=eng_tran_words[i+5][0], border=0)
-            pdf.set_x(pdf.get_x() - last_cell_w - last_cell_w) # Subtract cell_w twice because cell() increases pdf object's x property by w parameter automatically.
-
-        except (AttributeError, IndexError):
-            break
-
-    # Go to down one line and set cursor at right side of page.
-    pdf.set_y(pdf.get_y() + cell_h)
-    pdf.set_x(x_origin)
+        print_line_of_words_right_to_left(pdf, font, 10, cell_w, cell_h, eng_tran_words[i:i+6])
+        move_down_one_line(cell_h, x_origin)
 
 pdf.output('./pdfs/tanakh.pdf', 'F')
 
